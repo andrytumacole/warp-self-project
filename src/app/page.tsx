5be8e1f923c36ad888de86de6c13a9c1e6e6bb35
Andry T <andryjosephtumacole@gmail.com>
2025-06-26 11:55:17 +0800

    feat: basic layout of user avatar button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import UserAvatarButton from "@/components/user/user-avatar-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-  async function handleLogOut() {
-    await signOut();
-  }
-
   return (
     <div className="flex flex-col justify-center h-full items-center">
-      Logged in!!
-      <Button onClick={() => handleLogOut()}>Log out</Button>
+      <UserAvatarButton />
     </div>
   );
 }
--- src/components/user/user-avatar-button.tsx
+++ src/components/user/user-avatar-button.tsx
@@ -1,14 +1,36 @@
+"use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "../ui/button";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 function UserAvatarButton() {
-  return <></>;
+  const { signOut } = useAuthActions();
+  async function handleLogOut() {
+    await signOut();
+  }
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-20 hover:opacity-75 transition">
+          <AvatarImage />
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start" side="bottom" className="w-60">
+        <DropdownMenuItem>
+          <Button onClick={() => handleLogOut()}>Log out</Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default UserAvatarButton;
