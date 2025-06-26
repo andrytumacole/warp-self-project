a7551d39e3b59a070361d1df8cbdee8888821801
Andry T <andryjosephtumacole@gmail.com>
2025-06-26 16:43:05 +0800

    feat: added api hook to fetch workspace by id and integrated to frontend

---
+++ src/app/api/use-get-workspace-by-id.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { Id } from "../../../convex/_generated/dataModel";
+
+interface UseGetWorkspaceByIdProps {
+  id: Id<"workspaces">;
+}
+
+function useGetWorkspaceById(props: UseGetWorkspaceByIdProps) {
+  const { id } = props;
+  const workspace = useQuery(api.workspaces.getById, { id: id });
+  const isLoading = workspace === undefined;
+  return { workspace, isLoading };
+}
+
+export default useGetWorkspaceById;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import useGetWorkspaceById from "@/app/api/use-get-workspace-by-id";
 import useGetWorkspaceId from "@/hooks/use-get-workspace-id";
 
 function WorkspaceIdPage() {
   const workspaceId = useGetWorkspaceId();
+  const { workspace } = useGetWorkspaceById({ id: workspaceId });
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      ID: {workspaceId}
+    <div className="w-full h-full flex justify-center items-center p-40 flex-col">
+      <p>ID: {workspaceId}</p>
+      <div className="w-1/2">
+        <p className="text-wrap break-words">
+          Data: {JSON.stringify(workspace)}
+        </p>
+      </div>
     </div>
   );
 }
