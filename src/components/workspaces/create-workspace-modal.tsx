5578186e8d238620c9ac24e6a74e3c21eef675f8
Andry T <andryjosephtumacole@gmail.com>
2025-06-26 14:11:43 +0800

    feat: form fields for workspace creation modal

--- src/components/workspaces/create-workspace-modal.tsx
+++ src/components/workspaces/create-workspace-modal.tsx
@@ -4,28 +4,46 @@ import { useCreateWorkspaceModal } from "@/app/atom-states/use-create-workspace-
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { FormEvent, useState } from "react";
+
 function CreateWorkspaceModal() {
   const [isModalOpen, setIsModalOpen] = useCreateWorkspaceModal();
+  const [workspaceInfo, setWorkspaceInfo] = useState("");
 
   function handleClose() {
     setIsModalOpen(false);
   }
 
+  function handleCreate(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(workspaceInfo);
+  }
+
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
       <DialogContent className="rounded-lg [&>button]:hidden">
-        <DialogTitle>Create your own workspace</DialogTitle>
-        <DialogDescription>
-          Create a workspace before you proceed
-        </DialogDescription>
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
+        <form className="space-y-2.5" onSubmit={handleCreate}>
+          <Input
+            value={workspaceInfo}
+            required
+            autoFocus
+            minLength={3}
+            onChange={(e) => setWorkspaceInfo(e.target.value)}
+            placeholder={`Workspace name e.g. "Work", "Personal", "Home"`}
+          />
+          <div className="flex justify-end">
+            <Button type="submit">Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
