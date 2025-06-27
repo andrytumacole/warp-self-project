9b0af92d6159b26b626d3f943693402456c0e61f
Andry T <andryjosephtumacole@gmail.com>
2025-06-27 22:58:23 +0800

    feat: added get all members of workspace api endpoint

--- convex/membership_info.ts
+++ convex/membership_info.ts
@@ -1,6 +1,7 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
-import { query } from "./_generated/server";
-import { v } from "convex/values";
+import { MutationCtx, query, QueryCtx } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { Doc, Id } from "./_generated/dataModel";
 
 export const current = query({
   args: { workspaceId: v.id("workspaces") },
@@ -26,3 +27,63 @@ export const current = query({
     return member;
   },
 });
+
+export const get = query({
+  args: {
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    try {
+      const userId = await checkAuthorizedUser(ctx);
+      await checkAuthorizedUserRole(ctx, args.workspaceId, userId);
+    } catch {
+      return [];
+    }
+
+    const membershipInfos = await ctx.db
+      .query("membershipInfos")
+      .withIndex("by_workspace_id", (q) =>
+        q.eq("workspaceId", args.workspaceId)
+      )
+      .collect();
+
+    const members = [];
+
+    for (const memberInfo of membershipInfos) {
+      const user = await ctx.db.get(memberInfo.userId);
+      if (user) members.push({ ...memberInfo, user });
+    }
+
+    return members;
+  },
+});
+
+async function checkAuthorizedUser(ctx: MutationCtx | QueryCtx) {
+  const userId = await getAuthUserId(ctx);
+  if (!userId) {
+    throw new ConvexError({
+      message: "[client][workspace creation]: Unauthorized user",
+    });
+  }
+  return userId;
+}
+
+async function checkAuthorizedUserRole(
+  ctx: MutationCtx | QueryCtx,
+  workspaceId: Id<"workspaces">,
+  userId: Id<"users">
+) {
+  //query if the current user is a member of the args workspace
+  const membershipInfo = await ctx.db
+    .query("membershipInfos")
+    .withIndex("by_workspace_id_user_id", (q) =>
+      q.eq("workspaceId", workspaceId).eq("userId", userId)
+    )
+    .unique();
+
+  //user is not a member of the request workspace by id
+  if (!membershipInfo || membershipInfo.role !== "admin")
+    throw new ConvexError({
+      message: "[client][workspace preferences]: Unauthorized user",
+    });
+}
