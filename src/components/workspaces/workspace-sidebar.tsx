5eab4da303a2d8a3374757adf976ba893482c7c7
Andry T <andryjosephtumacole@gmail.com>
2025-06-27 12:37:17 +0800

    feat(workspace sidebar): integrated api endpoint to frontend

--- src/components/workspaces/workspace-sidebar.tsx
+++ src/components/workspaces/workspace-sidebar.tsx
@@ -1,5 +1,34 @@
+import useGetCurrentMember from "@/app/api/use-get-current-member";
+import useGetWorkspaceById from "@/app/api/use-get-workspace-by-id";
+import useGetWorkspaceId from "@/hooks/use-get-workspace-id";
+import { AlertTriangle, Loader } from "lucide-react";
+
 function WorkspaceSidebar() {
-  return <div>Workspace sidebar</div>;
+  const workspaceId = useGetWorkspaceId();
+  const { membershipInfo, isLoading: isFetchingMembershipInfo } =
+    useGetCurrentMember({ workspaceId: workspaceId });
+  const { workspace, isLoading: isFetchingWorkspace } = useGetWorkspaceById({
+    id: workspaceId,
+  });
+
+  if (isFetchingMembershipInfo || isFetchingWorkspace) {
+    return (
+      <div className="flex flex-col h-full items-center justify-center">
+        <Loader className="size-5 animate-spin" />
+      </div>
+    );
+  }
+
+  if (!workspace || !membershipInfo) {
+    return (
+      <div className="flex flex-col h-full items-center justify-center">
+        <AlertTriangle />
+        <p className="text-center">Workspace not found</p>
+      </div>
+    );
+  }
+
+  return <div className="flex flex-col h-full">Add header here</div>;
 }
 
 export default WorkspaceSidebar;
