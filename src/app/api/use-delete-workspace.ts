d99108b85e0c895211eb562733562e4858d0966b
Andry T <andryjosephtumacole@gmail.com>
2025-06-27 15:17:37 +0800

    feat: added hook to connect remove workspace api endpoint to the frontend

---
+++ src/app/api/use-delete-workspace.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { useConvexMutation } from "@convex-dev/react-query";
+import { api } from "../../../convex/_generated/api";
+
+type Options = {
+  onSuccess?: () => void;
+  onError?: () => void;
+  onSettled?: () => void;
+};
+function useRemoveWorkspace(options?: Options) {
+  const { mutateAsync, isPending, error } = useMutation({
+    mutationFn: useConvexMutation(api.workspaces.remove),
+    onSuccess: options?.onSuccess,
+    onError: options?.onError,
+    onSettled: options?.onSettled,
+  });
+
+  return { mutateAsync, isPending, error };
+}
+
+export default useRemoveWorkspace;
