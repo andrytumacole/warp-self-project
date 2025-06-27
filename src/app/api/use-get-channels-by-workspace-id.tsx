db30d60238770552a9e49099955de337dd6fb45d
Andry T <andryjosephtumacole@gmail.com>
2025-06-27 22:05:37 +0800

    feat: added api hook for get channels by workspace id endpoint

---
+++ src/app/api/use-get-channels-by-workspace-id.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { Id } from "../../../convex/_generated/dataModel";
+
+interface UseGetChannelsByWorkspaceIdProp {
+  workspaceId: Id<"workspaces">;
+}
+
+function useGetChannelsByWorkspaceId(
+  props: Readonly<UseGetChannelsByWorkspaceIdProp>
+) {
+  const { workspaceId } = props;
+  const channels = useQuery(api.channel.get, { workspaceId: workspaceId });
+  const isLoading = channels === undefined;
+  return { channels, isLoading };
+}
+
+export default useGetChannelsByWorkspaceId;
