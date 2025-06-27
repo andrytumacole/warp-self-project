19b4b51dc52ca6c78798b611ba6d4b3698e99451
Andry T <andryjosephtumacole@gmail.com>
2025-06-27 16:27:48 +0800

    feat(delete workspace): added confirm dialog and integrated api hook

---
+++ src/components/workspaces/delete-workspace-confirm-modal.tsx
@@ -0,0 +1,100 @@
+import useRemoveWorkspace from "@/app/api/use-delete-workspace";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { toast } from "sonner";
+import { Button } from "../ui/button";
+import { Id } from "../../../convex/_generated/dataModel";
+import { TrashIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface DeleteWorkspaceConfirmModalProp {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  workspaceName: string;
+  workspaceId: Id<"workspaces">;
+}
+
+function DeleteWorkspaceConfirmModal(
+  props: Readonly<DeleteWorkspaceConfirmModalProp>
+) {
+  const { isOpen, setIsOpen, workspaceName, workspaceId } = props;
+  const router = useRouter();
+
+  const {
+    mutateAsync: remove,
+    isPending: isRemovingWorkspace,
+    error: deleteError,
+  } = useRemoveWorkspace({
+    onSuccess: handleDeleteSuccess,
+    onError: handleDeleteError,
+    onSettled: handleDeleteSettled,
+  });
+
+  function handleDeleteSuccess() {
+    console.log("successfully deleted workspace");
+    toast("Successfully deleted workspace!", {
+      description: workspaceName,
+    });
+  }
+
+  function handleDeleteError() {
+    console.log("Something went wrong in deleting the workspace");
+    console.log("error: " + deleteError);
+  }
+
+  function handleDeleteSettled() {
+    console.log("Finished deleting!");
+    setIsOpen(false);
+    router.replace("/");
+  }
+
+  async function handleDelete() {
+    await remove({ id: workspaceId });
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger
+        className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600 outline-none"
+        asChild
+      >
+        <div>
+          <TrashIcon className="size-4" />
+          <p className="text-sm font-semibold">Delete workspace</p>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="[&>button]:hidden">
+        <DialogHeader>
+          <DialogTitle>Delete this workspace</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. Are you sure you want to do this?
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button
+            disabled={isRemovingWorkspace}
+            variant={"destructive"}
+            onClick={handleDelete}
+            className="focus-visible:outline-none"
+          >
+            Delete
+          </Button>
+          <DialogClose asChild>
+            <Button disabled={isRemovingWorkspace}>Cancel</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+export default DeleteWorkspaceConfirmModal;
--- src/components/workspaces/preferences-modal.tsx
+++ src/components/workspaces/preferences-modal.tsx
@@ -7,10 +7,9 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { TrashIcon } from "lucide-react";
-import { toast } from "sonner";
-import useRemoveWorkspace from "@/app/api/use-delete-workspace";
 import EditWorkspaceModal from "./edit-workspace-modal";
 import useGetWorkspaceId from "@/hooks/use-get-workspace-id";
+import DeleteWorkspaceConfirmModal from "./delete-workspace-confirm-modal";
 interface PreferencesModalProp {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
@@ -20,39 +19,14 @@ interface PreferencesModalProp {
 function PreferencesModal(props: Readonly<PreferencesModalProp>) {
   const { isOpen, setIsOpen, initialWorkspaceName } = props;
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+
   const [workspaceName, setWorkspaceName] = useState(initialWorkspaceName);
   const workspaceId = useGetWorkspaceId();
 
-  const {
-    mutateAsync: remove,
-    isPending: isRemovingWorkspace,
-    error: deleteError,
-  } = useRemoveWorkspace({
-    onSuccess: handleDeleteSuccess,
-    onError: handleDeleteError,
-    onSettled: handleDeleteSettled,
-  });
-
-  function handleDeleteSuccess() {
-    console.log("successfully deleted workspace");
-    toast("Successfully deleted workspace!", {
-      description: initialWorkspaceName,
-    });
-  }
-
-  function handleDeleteError() {
-    console.log("Something went wrong in deleting the workspace");
-    console.log("error: " + deleteError);
-  }
-
-  function handleDeleteSettled() {
-    console.log("Finished deleting!");
-    setIsOpen(false);
-  }
-
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="p-0 bg-gray-100 overflow-hidden [&>button]:hidden">
+      <DialogContent className="p-0 bg-gray-100 overflow-hidden [&>button]:hidden focus-visible:outline-none">
         <DialogHeader className="p-4 border-b bg-white">
           <DialogTitle>{workspaceName}</DialogTitle>
         </DialogHeader>
@@ -64,13 +38,12 @@ function PreferencesModal(props: Readonly<PreferencesModalProp>) {
             setWorkspaceName={setWorkspaceName}
             workspaceId={workspaceId}
           />
-          <button
-            onClick={() => {}}
-            className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600 outline-none"
-          >
-            <TrashIcon className="size-4" />
-            <p className="text-sm font-semibold">Delete workspace</p>
-          </button>
+          <DeleteWorkspaceConfirmModal
+            isOpen={isDeleteOpen}
+            setIsOpen={setIsDeleteOpen}
+            workspaceId={workspaceId}
+            workspaceName={workspaceName}
+          />
         </div>
       </DialogContent>
     </Dialog>
