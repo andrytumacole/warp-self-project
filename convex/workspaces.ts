1dce7033cafa91457065763d3b8a84875837255e
Andry T <andryjosephtumacole@gmail.com>
2025-06-27 16:52:44 +0800

    feat(workspace api): upgraded api endpoint to add one channel(general) to workspace upon creation

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -57,6 +57,11 @@ export const create = mutation({
       role: "admin",
     });
 
+    await ctx.db.insert("channels", {
+      name: "General",
+      workspaceId: workspaceId,
+    });
+
     return workspaceId;
   },
 });
