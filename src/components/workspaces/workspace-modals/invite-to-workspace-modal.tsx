174f51fe9d9053637964812db8fcc57e98e4a897
Andry T <andryjosephtumacole@gmail.com>
2025-06-28 19:55:46 +0800

    feat: generate new code integrated

--- src/components/workspaces/workspace-modals/invite-to-workspace-modal.tsx
+++ src/components/workspaces/workspace-modals/invite-to-workspace-modal.tsx
@@ -1,5 +1,6 @@
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogHeader,
@@ -8,17 +9,22 @@ import {
 import { toast } from "sonner";
 import { Button } from "../../ui/button";
 import { CopyIcon } from "lucide-react";
+import { Id } from "../../../../convex/_generated/dataModel";
+import RenewJoinCodeConfirmModal from "./renew-join-code-confirm-modal";
+import { useState } from "react";
 
 interface PreferencesModalProp {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
   workspaceName: string;
   joinCode: string;
-  workspaceId: string;
+  workspaceId: Id<"workspaces">;
 }
 
 function InviteToWorkspaceModal(props: Readonly<PreferencesModalProp>) {
   const { isOpen, setIsOpen, workspaceName, joinCode, workspaceId } = props;
+  const [isConfirmRenewModalOpen, setIsConfirmRenewModalOpen] = useState(false);
+
   async function handleCopy() {
     //origin means the domain where the app will be hosted
     const inviteLink = `${window.location.origin}/join/${workspaceId}`;
@@ -35,7 +41,7 @@ function InviteToWorkspaceModal(props: Readonly<PreferencesModalProp>) {
             Invite people to this workspace by using the code below
           </DialogDescription>
         </DialogHeader>
-        <div className="py-7 flex flex-col justify-center items-center gap-y-4">
+        <div className="pt-7 flex flex-col justify-center items-center gap-y-4">
           <p className="text-4xl font-bold tracking-widest uppercase">
             {joinCode}
           </p>
@@ -43,6 +49,16 @@ function InviteToWorkspaceModal(props: Readonly<PreferencesModalProp>) {
             Copy link
             <CopyIcon className="size-4 ml-2" />
           </Button>
+        </div>
+        <div className="flex items-center justify-between w-full px-5 py-4">
+          <RenewJoinCodeConfirmModal
+            isOpen={isConfirmRenewModalOpen}
+            setIsOpen={setIsConfirmRenewModalOpen}
+            workspaceId={workspaceId}
+          />
+          <DialogClose asChild>
+            <Button>Close</Button>
+          </DialogClose>
         </div>
       </DialogContent>
     </Dialog>
---
+++ src/components/workspaces/workspace-modals/renew-join-code-confirm-modal.tsx
@@ -0,0 +1,92 @@
+import { useRenewNewJoinCode } from "@/app/api/workspaces/use-renew-join-code";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { toast } from "sonner";
+import { Button } from "../../ui/button";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { RefreshCcw } from "lucide-react";
+
+interface RenewJoinCodeConfirmModallProp {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  workspaceId: Id<"workspaces">;
+}
+
+function RenewJoinCodeConfirmModal(
+  props: Readonly<RenewJoinCodeConfirmModallProp>
+) {
+  const { isOpen, setIsOpen, workspaceId } = props;
+
+  const {
+    data,
+    isPending: isRenewing,
+    error,
+    mutateAsync,
+  } = useRenewNewJoinCode({
+    onSuccess: handleRenewSuccess,
+    onError: handleRenewError,
+    onSettled: handleRenewSettled,
+  });
+
+  function handleRenewSuccess() {
+    toast("New code generated!", { description: data?.joinCode });
+  }
+
+  function handleRenewError() {
+    console.log("Something went wrong in updating the workspace");
+    console.log("error: " + error);
+  }
+
+  function handleRenewSettled() {
+    console.log("Finished renewing join code!");
+    setIsOpen(false);
+  }
+
+  async function handleRenew() {
+    await mutateAsync({ workspaceId: workspaceId });
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button variant={"outline"} disabled={isRenewing}>
+          New code
+          <RefreshCcw />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="[&>button]:hidden">
+        <DialogHeader>
+          <DialogTitle>Renew the join code</DialogTitle>
+          <DialogDescription>
+            This action will make the previous join codes obsolete. Are you sure
+            you want to do this?
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button
+            disabled={isRenewing}
+            onClick={handleRenew}
+            variant={"outline"}
+            className="focus-visible:outline-none"
+          >
+            Proceed
+          </Button>
+          <DialogClose asChild>
+            <Button disabled={isRenewing}>Cancel</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+export default RenewJoinCodeConfirmModal;
