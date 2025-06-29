3e3289e6ab35f2e84f0f385f220d2af50c257807
Andry T <andryjosephtumacole@gmail.com>
2025-06-29 19:12:36 +0800

    feat: only admins can edit workspace

--- src/components/channels/channel-header.tsx
+++ src/components/channels/channel-header.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import useGetChannelId from "@/hooks/use-get-channel-id";
 
-import { FaChevronDown } from "react-icons/fa";
+import { FaCog } from "react-icons/fa";
 import { Button } from "../ui/button";
 import {
   Dialog,
@@ -10,8 +11,9 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import EditChannelModal from "./channel-modals/edit-channel-modal";
-import useGetChannelId from "@/hooks/use-get-channel-id";
 import DeleteChannelModal from "./channel-modals/delete-channel-modal";
+import useGetCurrentMembershipInfo from "@/api/membership-infos/use-get-current-membership-info";
+import useGetWorkspaceId from "@/hooks/use-get-workspace-id";
 
 interface ChannelHeaderProps {
   initialChannelName: string;
@@ -20,45 +22,54 @@ interface ChannelHeaderProps {
 function ChannelHeader(props: Readonly<ChannelHeaderProps>) {
   const { initialChannelName } = props;
   const channelId = useGetChannelId();
+  const workspaceId = useGetWorkspaceId();
+  const { membershipInfo, isLoading: _isFetchingMembershipInfo } =
+    useGetCurrentMembershipInfo({
+      workspaceId: workspaceId,
+    });
   const [channelName, setChannelName] = useState(initialChannelName);
   const [isEditChannelModalOpen, setIsEditChannelModalOpen] = useState(false);
   const [isDeleteChannelModalOpen, setIsDeleteChannelModalOpen] =
     useState(false);
 
   return (
-    <div className="flex items-center px-4 bg-white border-b h-[49px] overflow-hidden">
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button
-            variant={"ghost"}
-            className="text-lg font-semibold px-2 overflow-hidden w-auto"
-            size={"sm"}
-          >
-            <span className="truncate"># {channelName}</span>
-            <FaChevronDown className="ml-1" />
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="p-0 bg-gray-50 overflow-hidden [&>button]:hidden">
-          <DialogHeader className="p-4 border-b bg-white">
-            <DialogTitle># {channelName}</DialogTitle>
-          </DialogHeader>
-          <div className="px-4 pb-4 flex flex-col gap-y-2">
-            <EditChannelModal
-              isOpen={isEditChannelModalOpen}
-              setIsOpen={setIsEditChannelModalOpen}
-              channelId={channelId}
-              initialChannelName={initialChannelName}
-              setChannelName={setChannelName}
-            />
-            <DeleteChannelModal
-              isOpen={isDeleteChannelModalOpen}
-              setIsOpen={setIsDeleteChannelModalOpen}
-              channelId={channelId}
-              channelName={channelName}
-            />
-          </div>
-        </DialogContent>
-      </Dialog>
+    <div className="flex items-center justify-between px-4 bg-white border-b h-[49px] overflow-hidden">
+      <div className="text-lg font-semibold px-2 overflow-hidden w-auto">
+        <span className="truncate"># {channelName}</span>
+      </div>
+      {membershipInfo?.role === "admin" && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              variant={"ghost"}
+              className="text-lg font-semibold p-4 overflow-hidden w-auto"
+              size={"lg"}
+            >
+              <FaCog className="text-slate-800" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-gray-50 overflow-hidden [&>button]:hidden">
+            <DialogHeader className="p-4 border-b bg-white">
+              <DialogTitle># {channelName}</DialogTitle>
+            </DialogHeader>
+            <div className="px-4 pb-4 flex flex-col gap-y-2">
+              <EditChannelModal
+                isOpen={isEditChannelModalOpen}
+                setIsOpen={setIsEditChannelModalOpen}
+                channelId={channelId}
+                initialChannelName={initialChannelName}
+                setChannelName={setChannelName}
+              />
+              <DeleteChannelModal
+                isOpen={isDeleteChannelModalOpen}
+                setIsOpen={setIsDeleteChannelModalOpen}
+                channelId={channelId}
+                channelName={channelName}
+              />
+            </div>
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   );
 }
